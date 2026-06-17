73420180384470c8924f2a8e493b750624899089
Dang Huynh Quyen <24130258@st.hcmuaf.edu.vn>
2026-06-17 16:57:24 +0700

    add thanh toán cọc và thanh toán xe

---
+++ src/main/java/com/carrental/model/enums/PaymentStatus.java
@@ -0,0 +1,8 @@
+package com.carrental.model.enums;
+
+public enum PaymentStatus {
+    PENDING,    // Đang chờ xử lý
+    SUCCESS,    // Thành công
+    FAILED,     // Thất bại
+    REFUNDED    // Đã hoàn tiền
+}
